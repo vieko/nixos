4d470fac2f7ff331de0a3ca6654a69534f578e70
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2022-10-14 13:40:05 -0600

    adds flag to enable flaes

--- default.nix
+++ default.nix
@@ -306,6 +306,7 @@
       options   = "--delete-older-than 7d";
     };
     trustedUsers = [ "root" "vieko" ];
+    settings.experimental-features = "nix-command flakes";
     # package = pkgs.nixFlakes;
     # registry.nixpkgs.flake = "nixpkgs/nixos-unstable";
     # extraOptions = ''
